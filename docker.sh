d0a7b96f8c562bfe43b7f2c3235ed8f437f06c24
pilbbq <53047591+pilbbq@users.noreply.github.com>
2019-12-10 14:51:11 +0100

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# VARIABLES
+#
+randomstring=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+sleeptime=1
+container="debian"
+containerversion='latest'
+containerfullname="${container}_${containerversion}_${randomstring}"
+containerpath="$container:$containerversion"
+containerhostname="docker-debian-container"
+#
+# SCRIPT
+#
+echo "####################################################################################################"
+echo "Running a new $container container version $containerversion interactively"
+echo "####################################################################################################"
+sleep $sleeptime
+sudo docker run --name $containerfullname -h $containerhostname -it $containerpath /bin/bash
+
+read -p "Do you wish to remove all docker containers ? (y/N) " RESP
+if [ "$RESP" = "y" ]; then
+  echo -e "Removing everything ... \n"
+  sudo docker container stop $(sudo docker container ls -aq)
+  sudo docker container prune -f
+else
+  echo -e "Exiting without removing anything \n"
+fi
