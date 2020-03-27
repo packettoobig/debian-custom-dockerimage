c9013c52ea87327fa06b186d1874db1a65f3aea5
pilbbq <53047591+pilbbq@users.noreply.github.com>
2020-03-27 11:44:32 +0100

    editing name

--- docker.sh
+++ docker.sh
@@ -8,7 +8,7 @@ container="debian"
 containerversion='latest'
 containerfullname="${container}_${containerversion}_${randomstring}"
 containerpath="$container:$containerversion"
-containerhostname="docker-debian-container"
+containerhostname="docker-$container-container-$randomstring"
 #
 # SCRIPT
 #
