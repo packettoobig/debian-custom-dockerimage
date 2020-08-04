ad198c825945ec4b24adf6fdfea08fbac526348d
pilbbq <53047591+pilbbq@users.noreply.github.com>
2020-08-04 10:11:00 +0200

    Clarify script user output, remove sleep

--- docker.sh
+++ docker.sh
@@ -4,7 +4,7 @@
 # VARIABLES
 #
 randomstring=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-sleeptime=1
+sleeptime=0
 container="debian"
 containerversion='latest'
 containerfullname="${container}_${containerversion}_${randomstring}"
@@ -19,13 +19,15 @@ echo "Running a new $container container version $containerversion interactively
 echo "####################################################################################################"
 sleep $sleeptime
 echo "Lauching $containerhostname"
-sudo docker run --name $containerfullname -h $containerhostname -it $containerpath /bin/bash
+sudo docker run --name $containerfullname -h $containerhostname \
+	-it $containerpath /bin/bash
 
-read -p "Do you wish to remove all docker containers and local data ? [y/N] " RESP
+sudo docker system df | awk '{print $1,$4}' | column -t
+read -p "Do you wish to remove all local docker data ? [y/N] " RESP
 if [ "$RESP" = "y" ] || [ "$RESP" = "yes" ]; then
   echo -e "Removing everything ... \n"
   sudo docker container stop $(sudo docker container ls -aq)
   sudo docker system prune -a -f
 else
-  echo -e "Exiting without removing anything \n"
+  echo "Exiting (nothing removed)"
 fi
